ecedac8e38f753de3b399fe9ef1fbc8ca24502af
JamalDunn23 <jamaldunn23@gmail.com>
2023-07-13 12:07:28 -0400

    Append form

--- aboutMe/app.js
+++ aboutMe/app.js
@@ -6,49 +6,26 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let li = document.createElement("li");
-  let name = form.querySelector("#name").value;
-  let price = form.querySelector("#price").value;
-  let inStockCheckbox = form.querySelector("#check-box");
-//   let inStock = inStockCheckbox.checked();
-  let image = form.querySelector("#img").value;
-  let shirtColor = form.querySelector("#shirt-color").value;
+  let firstName= form.querySelector("#first_name").value;
+  let lastName= form.querySelector("#last_name").value;
+  let planets= form.querySelector("#planets").value;
+  let species = form.querySelector("#species").value;
+  let transportation = form.querySelector("#transportation").value;
+  
 
   li.innerHTML = 
-    `<img src="${image}" alt="image" />
-    <h3>Name:</h3><p>${name}</p>
-    <h3>Price:</h3><p>${price}</p>
+    `<h3>First Name:</h3<p>${firstName}</p>
+    <h3>Last Name:</h3><p>${lastName}</p>
+    <h3>Planets:</h3><p>${planets}</p>
+    <h3>Species:</h3><p>${species}</p>
+    <h3>Transportation:</h3><p>${transportation}</p>`
     
-    <h3>Color:</h3><p>${shirtColor}</p>
-    <br/>
-    <button class="removeBtn">Remove</button>`;
 
   ul.append(li);
 
-  const deleteButton = li.querySelector(".removeBtn");
-  deleteButton.addEventListener("click", () => {
-    li.remove();
-  });
+ 
 
   form.reset();
 });
 
-let removeBtns = document.querySelectorAll(".removeBtn");
 
-removeBtns.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log("this is working");
-    const li = e.target.closest("li");
-    li.remove();
-  });
-});
-
-let removeExistingCheckboxes = document.querySelectorAll(".check-box");
-removeExistingCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", () => {
-           if (document.querySelector('.check-box').checked) {
-          checkbox.setAttribute('checked', 'checked');
-        } else {
-          checkbox.removeAttribute('checked');
-        }
-  });
-});
